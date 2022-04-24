4e87352a86a22557627e157a1c2878d242168baa
Jason Kwok <JasonHK@users.noreply.github.com>
2022-04-24 20:13:42 +0800

    Added boilerplate files

---
+++ app/src/main/java/app/jasonhk/hkcc/sleeptracker/MainActivity.java
@@ -0,0 +1,16 @@
+package app.jasonhk.hkcc.sleeptracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
