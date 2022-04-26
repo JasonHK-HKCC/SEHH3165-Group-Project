537dfdd5908ec7f676ce58c725a0b89a5aa888b4
kongp <ivy54828992@gmail.com>
2022-04-27 01:05:17 +0800

    about history

--- app/src/main/java/app/jasonhk/hkcc/sleeptracker/AnalysisFragment.java
+++ app/src/main/java/app/jasonhk/hkcc/sleeptracker/AnalysisFragment.java
@@ -11,8 +11,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.sql.Array;
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ServiceLoader;
 
 import lombok.val;
 
@@ -48,5 +53,32 @@ public class AnalysisFragment extends Fragment
                 .sum();
         TextView averageTimeText = view.findViewById(R.id.average_time);
         averageTimeText.setText(DataModel.formatDuration((totalSleeps == 0) ? 0 : totalTime / totalSleeps));
+
+        sessions = DataModel.getAllSessionsAfter(LocalDateTime.now().minusWeeks(1)).blockingGet()
+        List<SleepSession> time = new ArrayList<>();
+        SleepSession[] t1 = time.toArray(new SleepSession[0]);
+        SleepSession[] t2 = time.toArray(new SleepSession[1]);
+        SleepSession[] t3 = time.toArray(new SleepSession[2]);
+        SleepSession[] t4 = time.toArray(new SleepSession[3]);
+        SleepSession[] t5 = time.toArray(new SleepSession[4]);
+        SleepSession[] t6 = time.toArray(new SleepSession[5]);
+        SleepSession[] t7 = time.toArray(new SleepSession[6]);
+        TextView textView = view.findViewById(R.id.textView);
+        textView.setText(DataModel.formatDuration(t1.length));
+        TextView textView5 = view.findViewById(R.id.textView5);
+        textView5.setText(DataModel.formatDuration(t2.length));
+        TextView textView10 = view.findViewById(R.id.textView10);
+        textView10.setText(DataModel.formatDuration(t3.length));
+        TextView textView13 = view.findViewById(R.id.textView13);
+        textView13.setText(DataModel.formatDuration(t4.length));
+        TextView textView14 = view.findViewById(R.id.textView14);
+        textView14.setText(DataModel.formatDuration(t5.length));
+        TextView textView17 = view.findViewById(R.id.textView17);
+        textView17.setText(DataModel.formatDuration(t6.length));
+        TextView textView19 = view.findViewById(R.id.textView19);
+        textView19.setText(DataModel.formatDuration(t7.length));
+
+
+
     }
 }
